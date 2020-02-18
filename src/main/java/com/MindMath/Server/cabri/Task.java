18252253810c25922295fdf642b7597f6d500699
garbarini <delia.garbari@gmail.com>
2020-02-18 15:53:21 +0100

    JSON file post example (task)

--- src/main/java/com/MindMath/Server/cabri/Task.java
+++ src/main/java/com/MindMath/Server/cabri/Task.java
@@ -1,27 +1,58 @@
 package com.MindMath.Server.cabri;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Task {
 
-	private final long id;
-	private final String name;
-	private final String trigger;
+	private long id;
+	private String task;
+	private String trigger;
+	private int VT_2_1;
+	private int VT_2_2;
+	private boolean VT_2_3;
+	private boolean VT_2_4;
 
-	public Task(long id, String name, String trigger) {
+    @JsonProperty("params")
+    private void unpackNested(Map<String,Object> params) {
+		this.VT_2_1 = (int) params.get("VT-2.1");
+		this.VT_2_2 = (int) params.get("VT-2.2");
+		this.VT_2_3 = (boolean) params.get("VT-2.3");
+		this.VT_2_4 = (boolean) params.get("VT-2.4");
+    }
+
+	public Task(long id, String task, String trigger) {
 		this.id = id;
-		this.name = name;
+		this.task = task;
 		this.trigger = trigger;
 	}
 	
 	public long getId() {
 		return id;
 	}
-
-	public String getName() {
-		return name;
+	
+	public String getTask() {
+		return task;
 	}
 
 	public String getTrigger() {
 		return trigger;
 	}
-	
+
+	public float getVT_2_1() {
+		return VT_2_1;
+	}
+
+	public float getVT_2_2() {
+		return VT_2_2;
+	}
+
+	public boolean isVT_2_3() {
+		return VT_2_3;
+	}
+
+	public boolean isVT_2_4() {
+		return VT_2_4;
+	}
 }
--- src/main/java/com/MindMath/Server/cabri/Taskcontroller.java
+++ src/main/java/com/MindMath/Server/cabri/Taskcontroller.java
@@ -1,5 +1,18 @@
 package com.MindMath.Server.cabri;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class Taskcontroller {
 
+	@PostMapping(path = "/task", consumes = "application/json")
+	public String addtask(@RequestBody Task data) {
+		String message = "";
+		message += "id:" + data.getId() + "\n" + "task:" + data.getTask() + "\n" + "trigger:" + data.getTrigger() + "\n"
+				+ "VT-2.1:" + data.getVT_2_1() + "\n" + "VT-2.2:" + data.getVT_2_2() + "\n"
+				+ "VT-2.3:" + data.isVT_2_3() + "\n" + "VT-2.4:" + data.isVT_2_4() + "\n";
+		System.out.println(message);
+		
+		return "JSON file received by the server.";
+	}
 }
