2c72188dc423c4f974b7f4a96ceecdb4d586185f
ThibautSF <tsimonfine@gmail.com>
2020-02-21 13:30:08 +0100

    Add basic call (testing purpose)

---
+++ src/main/java/com/MindMath/learning/MainLearningProcess.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package com.MindMath.learning;
+
+/**
+ * @author tsimonfine
+ *
+ */
+public class MainLearningProcess {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO
+	}
+
+}
