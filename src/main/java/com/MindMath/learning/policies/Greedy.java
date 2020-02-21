6c4f36da637528b0db04e9b79aa71fefab8bf937
ThibautSF <tsimonfine@gmail.com>
2020-02-21 13:29:16 +0100

    Add policies
    - Greedy
    - Epsilon Greedy

---
+++ src/main/java/com/MindMath/learning/policies/Greedy.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package com.MindMath.learning.policies;
+
+import java.util.List;
+
+import com.MindMath.learning.utils.actions.IAction;
+import com.MindMath.learning.utils.values.IValue;
+
+/**
+ * @author tsimonfine
+ *
+ */
+public class Greedy implements IPolicy {
+
+	/**
+	 *
+	 */
+	public Greedy() {
+	}
+
+	@Override
+	public IAction chooseAction(List<IValue> values) {
+		int actionsCount = values.size();
+
+		// find best action (greedy)
+		IValue maxValue = values.get(0);
+
+		for (int i = actionsCount - 1; i >= 0; i--) {
+			IValue value = values.get(i);
+			if (value.getValue() > maxValue.getValue()) {
+				maxValue = value;
+			}
+		}
+
+		return maxValue.myAction();
+	}
+
+}
