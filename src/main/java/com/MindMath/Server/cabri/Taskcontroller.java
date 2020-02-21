1e647d79211a28e8266aa4e62774f432d3f96513
ThibautSF <tsimonfine@gmail.com>
2020-02-21 13:33:40 +0100

    add todo for qleanring call

--- src/main/java/com/MindMath/Server/cabri/Taskcontroller.java
+++ src/main/java/com/MindMath/Server/cabri/Taskcontroller.java
@@ -4,45 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Taskcontroller {
 
 	@Autowired
 	private Taskrepository taskrepository;
-	
+
 	@PostMapping(path = "/task", consumes = "application/json")
 	public String addtask(@RequestBody Task data) {
-		
-		if(!taskrepository.existsById(data.getId()))
-		{
+		if (!taskrepository.existsById(data.getId())) {
 			taskrepository.save(data);
 			System.out.println("Saved!\n");
 		}
 		String message = "";
 		message += "id:" + data.getId() + "\n" + "task:" + data.getTask() + "\n" + "trigger:" + data.getTrigger() + "\n"
-				+ "VT-2.1:" + data.getVT_2_1() + "\n" + "VT-2.2:" + data.getVT_2_2() + "\n"
-				+ "VT-2.3:" + data.isVT_2_3() + "\n" + "VT-2.4:" + data.isVT_2_4() + "\n";
+				+ "VT-2.1:" + data.getVT_2_1() + "\n" + "VT-2.2:" + data.getVT_2_2() + "\n" + "VT-2.3:"
+				+ data.isVT_2_3() + "\n" + "VT-2.4:" + data.isVT_2_4() + "\n";
 		System.out.println(message);
-		
+
+		// TODO call Q-learning algorithm
+
 		return "JSON file received by the server.";
 	}
-	
+
 	@GetMapping("/task")
 	public List<Task> getALLtask() {
-		
 		List<Task> tasks = new ArrayList<>();
-		taskrepository.findAll()
-		.forEach(tasks::add);
-		
+		taskrepository.findAll().forEach(tasks::add);
+
 		return tasks;
 	}
-	
+
 	@PutMapping(path = "/task")
 	public String updatetask(@RequestBody Task data) {
 		taskrepository.save(data);
-		//Q-learning algorithm
+
 		return "Task is updated. Q-learning algorithm is applied.";
 	}
 }
