4dd897638981d95e61eaf49e70142e84c00f9148
Yan Wang <yan.wang@lip6.fr>
2020-03-11 14:45:21 +0100

    update version of gson and httpclient

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,12 +13,12 @@ module com.mocah.mindmath {
 	requires spring.web;
 	requires java.desktop;
 	requires org.hibernate.orm.core;
-	requires gson;
-	requires httpclient;
 	requires org.apache.httpcomponents.httpcore;
 	requires org.apache.tomcat.embed.core;
 	requires spring.webmvc;
 	requires tuprolog;
+	requires org.apache.httpcomponents.httpclient;
+	requires com.google.gson;
 
 	opens com.mocah.mindmath.server;
 	opens com.mocah.mindmath.server.cabri;
