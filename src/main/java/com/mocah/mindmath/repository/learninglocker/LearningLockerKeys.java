e14f54c91e98418fcda0cfff7327edecf3d8c301
6ucky <hrbwy0451@gmail.com>
2020-07-07 11:33:18 +0200

    Update LRS auth code for production and test.

--- src/main/java/com/mocah/mindmath/repository/learninglocker/LearningLockerKeys.java
+++ src/main/java/com/mocah/mindmath/repository/learninglocker/LearningLockerKeys.java
@@ -11,7 +11,8 @@ public class LearningLockerKeys {
 	protected static String ABOUT_URL = "https://lrsmocah.lip6.fr/data/xAPI/about";
 	protected static String STATEMENT_URL = "https://lrsmocah.lip6.fr/data/xAPI/statements";
 	protected static String BASIC_AUTHORIZATION = "Authorization";
-	protected static String BASIC_AUTHORIZATION_VALUE = "Basic OWE5MDhjNGQwMDIyNTQ4ZmQyNTAxYzkxN2NkZGU1OTk1YzI2ODNmZDo0NDc3NGZlY2FhZmQ5NWVjN2U2ZjFiZmJlMDAzOGQxMzEyZmM2ODUy";
+	protected static String BASIC_AUTHORIZATION_VALUE = "Basic MzlhNTMwNGM1OGUxZGFlOTQ5ZmNiYjkxZWE4M2JkYjFhN2ViNzZkZjo1NjhjYzc2NzBhOTBkNjFhMTE3MDhkOTQ5MzVjYWRmNWE3YjE4M2M3";
+	protected static String BASIC_AUTHORIZATION_TEST_VALUE = "Basic MzU4MDRkYmI2NzFjZTNjOWJjYTFjODJkYTZlOGQ5NDEyMjE1OTI5MToyMWNiZTQ1ZDQzZTRlY2U2YmE3OTk0NDY5OTMwYjI5OTMxNDI3YmQ2";
 	protected static String BASIC_VERSION = "X-Experience-API-Version";
 	protected static String BASIC_VERSION_VALUE = "1.0.3";
 	
