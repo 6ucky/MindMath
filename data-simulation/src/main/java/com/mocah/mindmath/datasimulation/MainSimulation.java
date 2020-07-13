de78d00d1a289478acf37e92d5848387df72245a
ThibautSF <tsimonfine@gmail.com>
2020-07-13 12:28:23 +0200

    Init simulated data project

---
+++ data-simulation/src/main/java/com/mocah/mindmath/datasimulation/MainSimulation.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.mocah.mindmath.datasimulation;
+
+/**
+ * @author Thibaut SIMON-FINE
+ *
+ */
+public class MainSimulation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Stub de la méthode généré automatiquement
+
+	}
+
+}
---
+++ data-simulation/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+open module com.mocah.mindmath {
+
+	exports com.mocah.mindmath.datasimulation;
+
+	requires java.desktop;
+	requires com.google.gson;
+	requires com.google.common;
+
+	// test requirements
+//	requires org.junit.jupiter.api;
+}
