9d5c7e25c69d2af702ede28f7e4392ff165f58c3
6ucky <hrbwy0451@gmail.com>
2020-07-24 22:31:22 +0200

    ignore war context path

--- src/main/java/com/mocah/mindmath/server/ServerApplication.java
+++ src/main/java/com/mocah/mindmath/server/ServerApplication.java
@@ -2,13 +2,14 @@ package com.mocah.mindmath.server;
 
 import java.util.Collections;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-import com.google.common.base.Predicates;
 import com.mocah.mindmath.learning.LearningProcess;
 import com.mocah.mindmath.learning.TestLearningProcess;
 import com.mocah.mindmath.learning.algorithms.ILearning;
@@ -17,6 +18,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -54,9 +56,14 @@ public class ServerApplication extends SpringBootServletInitializer {
 
 	// Documentation of task API v1.0
 	@Bean
-	public Docket swaggerTaskApi1_0() {
+	public Docket swaggerTaskApi1_0(ServletContext servletContext) {
 		return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2).groupName("task-api-1.0")
-				.protocols(Collections.singleton("https")).host(hostbase).select()
+				.protocols(Collections.singleton("https")).host(hostbase).pathProvider(new RelativePathProvider(servletContext) {
+		              @Override
+		              public String getApplicationBasePath() {
+		                  return null;
+		              }
+		          }).select()
 				.apis(RequestHandlerSelectors.basePackage("com.mocah.mindmath.server.cabri"))
 				.paths(PathSelectors.ant("/task/**")).build()
 				.apiInfo(new ApiInfoBuilder().version("1.0").title("Task API from Cabri")
@@ -66,9 +73,14 @@ public class ServerApplication extends SpringBootServletInitializer {
 
 	// Documentation of Matrix API v1.0
 	@Bean
-	public Docket swaggerMatrixApi1_0() {
+	public Docket swaggerMatrixApi1_0(ServletContext servletContext) {
 		return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2).groupName("matrix-api-1.0")
-				.protocols(Collections.singleton("https")).host(hostbase).select()
+				.protocols(Collections.singleton("https")).host(hostbase).pathProvider(new RelativePathProvider(servletContext) {
+		              @Override
+		              public String getApplicationBasePath() {
+		                  return null;
+		              }
+		          }).select()
 				.apis(RequestHandlerSelectors.basePackage("com.mocah.mindmath.server.config"))
 				.paths(PathSelectors.regex("/matrix/v1.0.*")).build()
 				.apiInfo(new ApiInfoBuilder().version("1.0").title("Matrix API")
@@ -77,9 +89,14 @@ public class ServerApplication extends SpringBootServletInitializer {
 
 	// Documentation of Ontology API v1.0
 	@Bean
-	public Docket swaggerOntologyApi1_0() {
+	public Docket swaggerOntologyApi1_0(ServletContext servletContext) {
 		return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2).groupName("ontology-api-1.0")
-				.protocols(Collections.singleton("https")).host(hostbase).select()
+				.protocols(Collections.singleton("https")).host(hostbase).pathProvider(new RelativePathProvider(servletContext) {
+		              @Override
+		              public String getApplicationBasePath() {
+		                  return null;
+		              }
+		          }).select()
 				.apis(RequestHandlerSelectors.basePackage("com.mocah.mindmath.server.config"))
 				.paths(PathSelectors.regex("/ontology/v1.0.*")).build()
 				.apiInfo(new ApiInfoBuilder().version("1.0").title("Ontology API")
@@ -88,9 +105,14 @@ public class ServerApplication extends SpringBootServletInitializer {
 
 	// Documentation of LRS Learning Locker API
 	@Bean
-	public Docket swaggerLRSApi1_0() {
+	public Docket swaggerLRSApi1_0(ServletContext servletContext) {
 		return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2).groupName("lrs-api-1.0")
-				.protocols(Collections.singleton("https")).host(hostbase).select()
+				.protocols(Collections.singleton("https")).host(hostbase).pathProvider(new RelativePathProvider(servletContext) {
+		              @Override
+		              public String getApplicationBasePath() {
+		                  return null;
+		              }
+		          }).select()
 				.apis(RequestHandlerSelectors.basePackage("com.mocah.mindmath.server.config"))
 				.paths(PathSelectors.ant("/lrs/**"))
 //				.paths(Predicates.not(PathSelectors.ant("/lrs/test/**")))
@@ -100,9 +122,14 @@ public class ServerApplication extends SpringBootServletInitializer {
 	
 	// Documentation of feedback for Derby API
 	@Bean
-	public Docket swaggerFBApi1_0() {
+	public Docket swaggerFBApi1_0(ServletContext servletContext) {
 		return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2).groupName("fb-api-1.0")
-				.protocols(Collections.singleton("https")).host(hostbase).select()
+				.protocols(Collections.singleton("https")).host(hostbase).pathProvider(new RelativePathProvider(servletContext) {
+		              @Override
+		              public String getApplicationBasePath() {
+		                  return null;
+		              }
+		          }).select()
 				.apis(RequestHandlerSelectors.basePackage("com.mocah.mindmath.server.cabri.feedback")).paths(PathSelectors.any())
 				.build().apiInfo(new ApiInfoBuilder().version("1.0").title("Feedback API")
 						.description("Feedback content in Derby Documentation").contact(amel).build());
@@ -110,9 +137,14 @@ public class ServerApplication extends SpringBootServletInitializer {
 
 	// Documentation of file management API
 	@Bean
-	public Docket swaggerFileApi1_0() {
+	public Docket swaggerFileApi1_0(ServletContext servletContext) {
 		return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2).groupName("file-api-1.0")
-				.protocols(Collections.singleton("https")).host(hostbase).select()
+				.protocols(Collections.singleton("https")).host(hostbase).pathProvider(new RelativePathProvider(servletContext) {
+		              @Override
+		              public String getApplicationBasePath() {
+		                  return null;
+		              }
+		          }).select()
 				.apis(RequestHandlerSelectors.basePackage("com.mocah.mindmath.server.config"))
 				.paths(PathSelectors.ant("/file/**"))
 				.build().apiInfo(new ApiInfoBuilder().version("1.0").title("File API")
